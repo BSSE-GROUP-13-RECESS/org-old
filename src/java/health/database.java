178957abaf6ab65aef5bfe9a5e5bc4015c526e61
mwesigwajosh <joshuamj59@gmail.com>
2021-11-29 18:29:20 +0300

    final db class file

--- src/java/health/database.java
+++ src/java/health/database.java
@@ -4,10 +4,19 @@
  */
 package health;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Joshua
  */
 public class database {
-    
+        protected static Connection initializeDatabase()
+            throws SQLException, ClassNotFoundException{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection  con = DriverManager.getConnection("jdbc:mysql://localhost:3306/org","root","");
+            return con;
+        }
 }
